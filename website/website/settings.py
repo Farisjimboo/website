8e116c25a97f0cbaa51c0c181c4bdfabd35599ca
faris <mfariszaini96@gmail.com>
2019-09-25 04:31:02 +0000

    commit settings.py

--- website/website/settings.py
+++ website/website/settings.py
@@ -120,9 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, '../directapp/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, '../mainweb/static/')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+STATICFILES_DIRS = [os.path.join(BASE_DIR, '../mainweb/staticfiles/' )]
 
